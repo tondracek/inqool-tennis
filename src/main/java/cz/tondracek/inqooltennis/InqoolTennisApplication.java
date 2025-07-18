f798d2491d23e85412b94029f13af067fce7eeee
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-07-18 18:45:05 +0200

    Initial commit

---
+++ src/main/java/cz/tondracek/inqooltennis/InqoolTennisApplication.java
@@ -0,0 +1,13 @@
+package cz.tondracek.inqooltennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InqoolTennisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InqoolTennisApplication.class, args);
+    }
+
+}
