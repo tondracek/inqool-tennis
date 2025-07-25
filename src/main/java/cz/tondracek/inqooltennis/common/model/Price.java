fd5745ce54e2422b4e631c20cd6f6702abbf563b
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-07-25 21:51:16 +0200

    fix: swagger ui

--- src/main/java/cz/tondracek/inqooltennis/common/model/Price.java
+++ src/main/java/cz/tondracek/inqooltennis/common/model/Price.java
@@ -3,10 +3,9 @@ package cz.tondracek.inqooltennis.common.model;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.Currency;
 
 @Data
 public class Price {
     private BigDecimal amount;
-    private Currency currencyCode;
+    private String currencyCode;
 }
